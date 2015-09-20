7f18fbf75c6b86f856478d57cd53365b2d9466ea
nidhi mittal <nidhi.mittal@delhivery.com>
2015-09-20 15:27:40 +0530

    Added installable

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'pydynamo',
+    packages = ['pydynamo'],
+    version = '0.1.1',
+    description = 'Python library for communicating with dynamo db !',
+    author='Nidhi Mittal',
+    author_email='mittalnidhi.mittal@gmail.com',
+    url='https://github.com/nidhi-delhivery/pydynamo',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
